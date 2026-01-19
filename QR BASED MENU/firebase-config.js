57b7638e9be0a6a6d5caf81ccbba67c13e2d5d19
Prince kumar <monsterprince3633@gmail.com>
2026-01-19 08:30:12 +0530

    Update firebase-config.js

--- QR BASED MENU/firebase-config.js
+++ QR BASED MENU/firebase-config.js
@@ -1,8 +1,9 @@
 const firebaseConfig = {
   apiKey: "AIzaSyAY_TN3to4yAqk7IJNqVSKFS-fSuynXwOw",
-  authDomain: "teste-erhij8.firebaseapp.com",
+  authDomain: "Prince364133.github.io",
   projectId: "teste-erhij8",
   storageBucket: "teste-erhij8.firebasestorage.app",
   messagingSenderId: "744778225564",
   appId: "1:744778225564:web:f5d6b73e501495b3dfda59"
 };
+
